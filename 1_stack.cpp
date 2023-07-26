4b7e7031ea266713fca97bab9d5c702b6ba51ea6
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-27 00:01:37 +0530

    Stack

--- 1_stack.cpp
+++ 1_stack.cpp
@@ -2,79 +2,64 @@
 using namespace std;
 #define size 5
 
-class stack
-{
+class stack {
     int stack[size];
     int top;
-    public:
-        void init();
-        void push();
-        int pop();
-        void display();
+
+public:
+    void init() {
+        top = 0;
+    }
+    void push() {
+        int i;
+        if (top == size) {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        cout << "Enter the element" << endl;
+        cin >> i;
+        stack[top] = i;
+        top++;
+    }
+    int pop() {
+        if (top == 0)
+        {
+            cout << "Stack Underflow \n";
+            return 1;
+        }
+        top--;
+        cout << "Popped element is " << stack[top] << endl;
+        return 0;
+    }
+    void display() {
+        if (top == 0)
+        {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        for (int i = 0; i < top; i++)
+        {
+            cout << stack[i] << " ";
+        }
+        cout << endl;
+    }
 };
 
-void stack::init()
-{
-    top = 0;
-}
-
-void stack::push()
-{
-    int i;
-    if (top == size)
-    {
-        cout<<"Stack Overflow"<<endl;
-        return;
-    }
-    cout <<"Enter the element"<<endl;
-    cin>>i;
-    stack[top] = i;
-    top++;
-}
-
-int stack::pop()
-{
-    if (top == 0)
-    {
-        cout <<"Stack Underflow \n";
-        return 1;
-    }
-    top--;
-    cout<<"Popped element is "<<stack[top]<<endl;
-    return 0;
-}
-
-void stack::display()
-{
-    if (top == 0)
-    {
-        cout <<"Stack is empty"<<endl;
-        return;
-    }
-    for (int i = 0; i < top; i++)
-    {
-        cout<<stack[i]<<" ";
-    }
-    cout<<endl;
-}
-
-int main()
-{
+int main() {
     int ch, i;
     stack s;
     s.init();
-    while (1)
-    {
-        cout<<endl<<"Select the Operation"<<endl;
-        cout<<"--------------------"<<endl;
-        cout <<"1. Push\n2. Pop\n3. Display\n";
-        cin>>ch;
-        switch (ch)
-        {
+    while (1) {
+        cout <<endl<< "Select the Operation"<<endl;
+        cout <<"--------------------"<<endl;
+        cout<<"1. Push\n2. Pop\n3. Display\n4. Exit\n";
+        cin >> ch;
+        switch (ch) {
             case 1: s.push(); break;
             case 2: s.pop(); break;
             case 3: s.display(); break;
-            default: return 0;
+            case 4: return 0;
+            default: cout<<"Invalid Input...!";
         }
     }
 }
