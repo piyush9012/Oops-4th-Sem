38fd79e724e28d144ef61959a886a8c90eada3d7
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-27 22:55:23 +0530

    List

--- 3_List.cpp
+++ 3_List.cpp
@@ -15,61 +15,62 @@ class list
         list(){
             f = NULL;
         }
-        void ins(int num){
+        void insert()
+        {
+            int num;
+            cout<<"Enter the number"<<endl;
+            cin>>num;
             node *p = new node;
             p->data = num;
             p->next = f;
             f = p;
         }
-        void del(){
+
+        void delet()
+        {
             node *temp = f;
             if (f == NULL)
                 cout<<"No elements to delete\n"<<endl;
-            else{
+            else
+            {
                 cout <<"The deleted elements is: "<<f->data<<endl;
                 f = f->next;
                 delete temp;
                 cout<<"Deletion successfull"<<endl;
             }
-            return;
         }
-        void disp(){
+
+        void display()
+        {
             node *temp = f;
             if (f == NULL)
                 cout<<"List is empty"<<endl;
             else{
                 cout<<"Elements in the list are: ";
                 while (temp != NULL){
-                    cout<<" "<<temp->data;
+                    cout<<temp->data<<" ";
                     temp = temp->next;
                 }
-                cout<<endl<<endl;
+                cout<<endl;
             }
         }
 };
 
 int main()
 {
-    int num, ch=1;
+    int num, ch;
     list ob;
-    cout<<endl<<"Select the Operation"<<endl;
-    cout<<"--------------------"<<endl;
-    cout<<"1. Insert\n2. Delete\n3. Exit"<<endl;
-    while (ch){
+    while (1){
+        cout<<endl<<"Select the Operation"<<endl;
+        cout<<"--------------------"<<endl;
+        cout<<"1. Insert\n2. Delete\n3. Display\n4. Exit"<<endl;
         cout<<"Enter your choice"<<endl;
         cin>>ch;
         switch (ch){
-            case 1:
-                cout<<"Enter the number"<<endl;
-                cin>>num;
-                ob.ins(num);
-                ob.disp();
-                break;
-            case 2:
-                ob.del();
-                ob.disp();
-                break;
-            case 3: return 0;
+            case 1: ob.insert();  break;
+            case 2: ob.delet();   break;
+            case 3: ob.display(); break;
+            case 4: return 0;
             default: cout <<"Invalid choice \n"; break;
         }
     }
